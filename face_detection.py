b37f288185dd75622116fdf126646e4e406f5766
shank885 <shashankshekhar885@gmail.com>
2018-06-25 14:59:06 +0530

    Added Exception Handling

--- face_detection.py
+++ face_detection.py
@@ -1,108 +1,136 @@
-import requests
-import cv2 
+import requests 
 import json
 import csv
-#from io import BytesIO
-#from PIL import Image, ImageDraw
-#from matplotlib import patches
-#import numpy as np 
-#import cognitive_face as CF
+import cv2
 
-#create a VideoCapture object and a window "Captured Image"
-cam = cv2.VideoCapture(0)
-cv2.namedWindow("Captured Image")
+def clickPhoto():
+	#create a VideoCapture object and a window "Captured Image"
+	cam = cv2.VideoCapture(0)
+	cv2.namedWindow("Captured Frame")
+
+	while True:
+		if(cam.isOpened()):
+			# Read frame from camera
+			flag, frame = cam.read()
+			cv2.imshow("Captured Frame", frame)
+
+		if not flag:
+			break
+		k = cv2.waitKey(1)
+
+		if k%256 == 27:
+			#ESC pressed ----- Break
+			print("!!Escape Hit!!, Closing.....")
+			return 0
+		elif k%256 == 32:
+			#SPACE pressed ---  Save Image
+			img_name = "Image_0.jpg"
+			cv2.imwrite(img_name, frame)
+			print("..........{} Written!!.........".format(img_name))
+			print("..........Loading Captured Image........")
+			#Release cam object and close all image windows
+			cam.release()
+			cv2.destroyWindow("Captured Frame")
+			return 1
 
 
-while True:
-	if(cam.isOpened()):
-		# Read frame from camera
-		flag, frame = cam.read()
-	cv2.imshow("Captured Image", frame)
+def getFaceData(subscription_key, face_api_url, image_path):
 
-	if not flag:
-		break
-	k = cv2.waitKey(1)
+	assert subscription_key
+	# Read Image into byte array(use absolute address)
+	image_data = open(image_path, "rb").read()
+	# header for reading local image
+	headers = {'Ocp-Apim-Subscription-Key': subscription_key,
+			   'Content-Type': 'application/octet-stream'}
+	# set parameter to be passed and received
+	params = {
+    	'returnFaceId': 'true',
+    	'returnFaceLandmarks': 'false',
+    	'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,' +
+    	'emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
+	}
+	# Send Image and Get Response with error handling:
+	try:
+		response = requests.post(face_api_url, 
+								headers=headers, 
+								params=params, 
+								data=image_data
+								)
+		response.raise_for_status()
+		faces = response.json()
+		print("Face Data Received")
+		return faces
+	except requests.exceptions.HTTPError as errh:
+		print ("Http Error:",errh)
+	except requests.exceptions.ConnectionError as errc:
+		print ("Error Connecting:",errc)
+	except requests.exceptions.Timeout as errt:
+		print ("Timeout Error:",errt)
+	except requests.exceptions.RequestException as err:
+		print ("OOps: Something Else",err)
+	return None
+	
 
+def saveFaceData(faces):
+	# save face data to csv file "face_data.csv"
+	data = faces
+	f = open('face_data.csv','w')
+	csv_file = csv.writer(f)
+	csv_file.writerow(data[0].keys())
+	for item in data:
+		csv_file.writerow(item.values())
+	f.close()
+	with open('face_data_json.txt','w') as outfile:
+		json.dump(faces, outfile, indent=2, sort_keys = True, ensure_ascii = False)
+	outfile.close()
+	print("Face Data Saved")								
+
+
+def printFaceData(faces):
+	
+	face_count = 0
+	for item in faces:
+		face_count += 1
+		face_name = "FACE :{}".format(face_count)
+		print(face_name,"\n")
+		print(item['faceAttributes']['gender'])
+		print(item['faceAttributes']['age'])
+		emotion_conf = 0
+		emo_name = " "
+		for emo in item['faceAttributes']['emotion']:
+			if item['faceAttributes']['emotion'][emo] > emotion_conf:
+				emotion_conf = item['faceAttributes']['emotion'][emo]
+				emo_name = emo
+		print(emo_name)
+		print("%s percentage : %s"% (emo_name, emotion_conf*100),"\n")
+	print("Face Data Printed")
+
+
+def showImage():
+
+	img = cv2.imread("Image_0.jpg")
+	cv2.imshow("Captured Image", img)
+	k = cv2.waitKey(0)
+	# press escape to close all image windows
 	if k%256 == 27:
-		#ESC pressed ----- Break
-		print("!!Escape Hit!!, Closing.....")
-		print("Loading Previous Image......")
-		break
-	elif k%256 == 32:
-		#SPACE pressed ---  Save Image
-		img_name = "Image_0.jpg"
-		cv2.imwrite(img_name, frame)
-		print("{} Written!!".format(img_name))
-		print("Loading Captured Image........")
-		break
-
-#Release cam object and close all image windows
-cam.release()
-cv2.destroyAllWindows()
+		cv2.destroyAllWindows()
+	print("Image Shown")
 
 
 # Assign subscription key
 subscription_key = "ea2c5997ddac418980aebbaf569c34ca"
-assert subscription_key
-
 # Accessing face api url
 face_api_url = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect/"
-
 # set image path to local address
 image_path = "/home/shashank/Face_Detection/Image_0.jpg"
 
-# Read Image into byte array(use absolute address)
-image_data = open(image_path, "rb").read()
-
-
-# header for reading local image
-headers = {'Ocp-Apim-Subscription-Key': subscription_key,
-		   'Content-Type': 'application/octet-stream'}
-
-# set parameter to be passed and received
-params = {
-    'returnFaceId': 'true',
-    'returnFaceLandmarks': 'false',
-    'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,' +
-    'emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
-}
-
-# Send Image and Get Response
-response = requests.post(
-    face_api_url, headers=headers, params=params, data=image_data)
-faces = response.json()
-
-# save face data to csv file "face_data.csv"
-data = faces
-f = open('face_data.csv','w')
-csv_file = csv.writer(f)
-csv_file.writerow(data[0].keys())
-for item in data:
-	csv_file.writerow(item.values())
-f.close()
-
-# Display the originl imag and overlay it with the face information:
-#res_json = json.loads(response.content.decode('utf-8'))
-#print(json.dumps(res_json, indent = 2, sort_keys = True))
-
-with open('face_data_json.txt','w') as outfile:
-	json.dump(faces, outfile, indent=2, sort_keys = True, ensure_ascii = False)
-
-for item in faces:
-	my_dict = {}
-	my_dict['Gender'] = item['faceAttributes']['gender']
-	#my_dict['Gender'] = item.get('faceAttributes').get('gender')
-	my_dict['Age'] = item['faceAttributes']['age']
-	emotion_conf = 0
-	emo_name = " "
-	for emo in item['faceAttributes']['emotion']:
-		if item['faceAttributes']['emotion'][emo] > emotion_conf:
-			emotion_conf = item['faceAttributes']['emotion'][emo]
-			emo_name = emo
-	my_dict['Emotion'] = emo_name
-	print("%s : %s"% (emo_name, emotion_conf))
-	#my_dict['Emotions'] = item['faceAttributes']['emotion']
-
-for (k,v) in my_dict.items():
-	print("%s : %s"% (k, v)) 
-
+# call clickPhoto() and subscequent functions
+click_flag = clickPhoto()		
+if click_flag:
+	faces = getFaceData(subscription_key, face_api_url, image_path)
+	if faces:
+		saveFaceData(faces)
+		printFaceData(faces)
+		showImage()
+else:
+	print("!!!!!!!!!!No Photo Chaptured!!!!!!!!!")
